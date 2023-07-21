fd2ba5ab6bb41f877852355c2bdea0c9043b419b
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-07-22 00:49:54 +0300

    start

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+image = require('./image/Photo.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <Text>Open up App.js to start working on your app!</Text>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    width: 375,
   },
 });
